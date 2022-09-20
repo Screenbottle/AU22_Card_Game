49afd0a1050a5210ae0fe40a8d8206357bce84cb
Arvid <arvid.rydh@iths.se>
2022-09-20 14:52:02 +0200

    added card assets

---
+++ app/src/main/java/com/example/au22_card_game/RoundActivity.kt
@@ -0,0 +1,11 @@
+package com.example.au22_card_game
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RoundActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_round)
+    }
+}
